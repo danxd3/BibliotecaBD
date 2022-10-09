49c2d97b16b0f38eeedc9e03d43e334fa7b5200e
Zerg <Zerg@DESKTOP-JFTEAER>
2022-10-08 21:40:33 -0500

    Agregar archivos de proyecto.

---
+++ BibliotecaBD/Beer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliotecaBD
+{
+    public partial class Beer
+    {
+        public Beer()
+        {
+            InverseBrand = new HashSet<Beer>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int BrandId { get; set; }
+
+        public virtual Beer Brand { get; set; } = null!;
+        public virtual ICollection<Beer> InverseBrand { get; set; }
+    }
+}
---
+++ BibliotecaBD/CursoPubContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BibliotecaBD
+{
+    public partial class CursoPubContext : DbContext
+    {
+        public CursoPubContext()
+        {
+        }
+
+        public CursoPubContext(DbContextOptions<CursoPubContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Beer> Beers { get; set; } = null!;
+        public virtual DbSet<Brand> Brands { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CursoPub;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beer>(entity =>
+            {
+                entity.ToTable("Beer");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.BrandId).HasColumnName("BrandID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Brand)
+                    .WithMany(p => p.InverseBrand)
+                    .HasForeignKey(d => d.BrandId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Beer_Beer");
+            });
+
+            modelBuilder.Entity<Brand>(entity =>
+            {
+                entity.ToTable("Brand");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
